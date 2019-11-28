be6523fcbd00e0f6bf8553b50b8d90c358eb0fbd
eliana.vargas <eliana.vargas@quipux.com>
2019-11-27 20:58:59 -0500

    Se modifica componentes ingresos para conexión con el back

--- src/components/ProfileFormIngresos.js
+++ src/components/ProfileFormIngresos.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 
-class ProfileFormComponent extends React.Component{
+class ProfileFormComponent extends React.Component {
     state = {};
 
     handleClick = e => {
@@ -13,60 +13,60 @@ class ProfileFormComponent extends React.Component{
         console.log("Form submit")
         console.log(this.props.formValues)
         axios.post('https://finanzas-app.mileidyramos23171.now.sh/api/ingresos/', this.props.formValues)
-        .then(profile => alert('Profile create <3'))
-        .catch(err => alert(err))
+            .then(profile => alert('Profile create <3'))
+            .catch(err => alert(err))
     };
 
-    render(){
-        return(
+    render() {
+        return (
             <React.Fragment>
                 <h1>Nuevo Ingreso</h1>
 
                 <form onSubmit={this.handleSubmit} >
                     <div className="form-group">
                         <label>Fecha</label>
-                        <input 
-                            onChange={this.props.onChange} 
-                            className="form-control" 
-                            type="text" 
-                            name="firstName"
+                        <input
+                            onChange={this.props.onChange}
+                            className="form-control"
+                            type="text"
+                            name="fecha"
                             value={this.props.formValues.fecha}
                         />
                     </div>
                     <div className="form-group">
                         <label>Valor del ingreso</label>
-                        <input 
-                            onChange={this.props.onChange} 
-                            className="form-control" 
-                            type="text" 
-                            name="lastName"
+                        <input
+                            onChange={this.props.onChange}
+                            className="form-control"
+                            type="text"
+                            name="valoringreso"
                             value={this.props.formValues.valoringreso}
                         />
                     </div>
                     <div className="form-group">
-                    <label>Categoria ingreso</label>
-                        <select className="custom-select d-block w-100">
-                        <option value="">Seleccione una categoria</option>
-                        <option value="categoria">Fijo</option>
-                        <option>Extra</option>
+                        <label>categoria Ingreso</label>
+                        <select className="custom-select d-block w-100" name="categoria">
+                            <option value="">Seleccione una categoria</option>
+                            <option value={this.props.formValues.categoria}>Nomina</option>
+                            <option value={this.props.formValues.categoria}>Estras</option>
                         </select>
                     </div>
                     <div className="form-group">
                         <label>Descripción del ingresos</label>
-                        <input 
-                            onChange={this.props.onChange} 
-                            className="form-control" 
-                            type="text" 
-                            name="jobTitle"
+                        <input
+                            onChange={this.props.onChange}
+                            className="form-control"
+                            type="text"
+                            name="descripcioningreso"
                             value={this.props.formValues.descripcioningreso}
                         />
                     </div>
                     <div className="form-group">
                         <label>Twitter</label>
-                        <input 
-                            onChange={this.props.onChange} 
-                            className="form-control" 
-                            type="text" 
+                        <input
+                            onChange={this.props.onChange}
+                            className="form-control"
+                            type="text"
                             name="twitter"
                             value={this.props.formValues.twitter}
                         />
--- src/pages/ProfileIngresos.js
+++ src/pages/ProfileIngresos.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import ProfileComponent from '../components/ProfileComponent'
-import ProfileForm from '../components/ProfileFormIngresos'
+import ProfileComponent from '../components/ProfileComponent';
+import ProfileForm from '../components/ProfileFormIngresos';
 import '../styles/ProfileNew.css'
 import axios from 'axios';
 
-class ProfileNew extends React.Component{
+class ProfileNew extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             form: {
-                fecha:'',
-                valoringreso:'',
-                categoria:'',
-                descripcioningreso:'',
-                twitter:''
+                fecha: '',
+                valoringreso: '',
+                categoria: '',
+                descripcioningreso: '',
+                twitter: ''
             }
         }
     }
 
-    handleChange = e =>{
+    handleChange = e => {
         this.setState({
             form: {
                 ...this.state.form,
@@ -28,26 +28,25 @@ class ProfileNew extends React.Component{
         })
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <React.Fragment>
-                
+
                 <div className="container">
                     <div className="row">
                         <div className="col-6">
-                            <ProfileComponent     
-                                        fecha = {this.state.form.fecha} 
-                                        valoringreso = {this.state.form.valoringreso} 
-                                        categoria = {this.state.form.categoria} 
-                                        descripcioningreso = {this.state.form.descripcioningreso}
-                                        twitter = {this.state.form.twitter}  
-                                        avatarUrl = "https://s3.us-east-2.amazonaws.com/eafitrequisitos/avataaars.png"
+                            <ProfileComponent
+                                fecha={this.state.form.fecha}
+                                valoringreso={this.state.form.valoringreso}
+                                categoria={this.state.form.categoria}
+                                descripcioningreso={this.state.form.descripcioningreso}
+                                twitter={this.state.form.twitter}
                             />
                         </div>
                         <div className="col-6">
-                            <ProfileForm 
+                            <ProfileForm
                                 onChange={this.handleChange}
-                                formValues={this.state.form} 
+                                formValues={this.state.form}
                             />
                         </div>
                     </div>
